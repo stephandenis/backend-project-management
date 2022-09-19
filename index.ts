b90026d32c80f0787d5f72f65008d0bed8ecb555
Stephan Denis <stephangdenis@gmail.com>
2022-09-19 13:40:42 -0300

    server

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/db';
+import { typeDefs } from './graphql/types';
+import { resolvers } from './graphql/resolvers';
+
+dotenv.config();
+
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen({ port: process.env.PORT || 4000 }, async () => {
+  await connectDB();
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  console.log('Server Ready 🚀');
+});
